91f8b6197e6f4b2859c92bd8b5c379c8791928e8
Borna <borna.grilec@speck.agency>
2020-04-27 22:48:36 +0200

    Created Articles component

---
+++ src/components/Articles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Article from './Article';
+
+const Articles = (props) => {
+    return (
+        <>
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+        </>
+    );
+}
+
+export default Articles;
